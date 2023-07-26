5a61602a7e33991693275162b86b9be779be4ae5
joshua-mcgehee <joshua.mcgehee@generalassemb.ly>
2023-07-26 16:54:25 -0600

    Post Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 // import Link from "next/link";
-// import { api } from "~/utils/api";
+import { api } from "~/utils/api";
 
 export default function Home() {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -20,7 +21,11 @@ export default function Home() {
           {!!user.isSignedIn && <SignOutButton />}
           {/* this is still displaying with no user */}
         </div>
-        <SignIn />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
